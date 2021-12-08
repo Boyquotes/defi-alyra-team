6a3f22e804b0bd306a88c085b19b452091bab5da
Thiou21 <83233428+Thiou21@users.noreply.github.com>
2021-12-08 04:34:21 +0100

    Add files via upload

---
+++ periphery/truffle-config.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+module.exports = {
+  
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gasPrice: 0x01,
+    },
+},
+
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+             runs: 200
+         },
+       }
+     },
+    },
+  };
